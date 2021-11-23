e74d6ededce9dbe7e7049c2d2cae30757fbf782b
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2021-11-23 20:16:02 +1100

    Update with latest espanso conf. Make task compatible with mac

--- scripts/encrypt-espanso-conf.sh
+++ scripts/encrypt-espanso-conf.sh
@@ -6,4 +6,6 @@ elif [[ "$OSTYPE" = "darwin"* ]] ; then
 	ESPANSO_CONF=$HOME/Library/Preferences/espanso/default.yml
 fi
 
+printf "%s %s\n" "Encrypting conf at:" $ESPANSO_CONF
+
 ansible-vault encrypt $ESPANSO_CONF --output ./.espanso/default.yml
